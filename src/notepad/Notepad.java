ef730f1709e02eeebff4ed82a80967537e0b8158
Satvik Khare <satvikkhare2002@gmail.com>
2024-07-19 23:35:47 +0530

    first commit

---
+++ src/notepad/Notepad.java
@@ -0,0 +1,12 @@
+package notepad;
+
+import javax.swing.*;
+
+public class Notepad extends JFrame {
+    Notepad(){
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+
+    }
+}
